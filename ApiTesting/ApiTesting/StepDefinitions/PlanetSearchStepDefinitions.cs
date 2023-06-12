22bc98ea18dfe7b67b3302a39b5e527473b3351d
laszlogallay <laszlo_gallay@epam.com>
2023-06-12 14:16:43 +0200

    First TC created

---
+++ ApiTesting/ApiTesting/StepDefinitions/PlanetSearchStepDefinitions.cs
@@ -0,0 +1,45 @@
+using RestSharp;
+using System;
+using System.Net;
+using System.Text;
+using TechTalk.SpecFlow;
+using Newtonsoft.Json;
+using RestSharp.Serializers.NewtonsoftJson;
+using ApiTesting.Support.JsonObjects;
+
+namespace ApiTesting.StepDefinitions
+{
+    [Binding]
+    public class PlanetSearchStepDefinitions
+    {
+        RestResponse PlanetResponse;
+
+        [Given(@"the planet ID is: (.*)")]
+        public async Task GivenThePlanetIDIsAsync(int planetId)
+        {
+            string BaseURL = "https://swapi.dev/api/";
+            string endpoint = BaseURL + "planets/" + planetId;
+            var Client = new RestClient(endpoint, configureSerialization: s => s.UseNewtonsoftJson());
+            RestRequest Request = new RestRequest(endpoint) { Method = Method.Get };
+            Request.AddHeader("Content-Type", "application/json");
+            Request.AddHeader("Accept", "application/json");
+            PlanetResponse = await Client.GetAsync(Request);
+        }
+
+        [When(@"response status is: (.*)")]
+        public void WhenResponseStatusIs(string responseStatus)
+        {
+            Assert.Equal(responseStatus, PlanetResponse.StatusCode.ToString());
+        }
+
+        [Then(@"the planet name is: '([^']*)'")]
+        public void ThenThePlanetNameIs(string planetName)
+        {
+            string ResponseContent = PlanetResponse.Content;
+            Planet PlanetData = JsonConvert.DeserializeObject<Planet>(ResponseContent);
+
+            string PlanetName = PlanetData.name;
+            Assert.True(PlanetName.Equals(planetName));
+        }
+    }
+}
